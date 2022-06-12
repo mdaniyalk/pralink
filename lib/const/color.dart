f8c6ccfa321c7908bd4e6358cee20bb0ab551011
mdaniyalk <88580903+mdaniyalk@users.noreply.github.com>
2022-06-12 14:25:34 +0700

    Adding new color constant

---
+++ lib/const/color.dart
@@ -0,0 +1,6 @@
+Color bluecolor = Color(0xff418FDE);
+Color redcolor = Color(0xffFF5668);
+Color orangecolor = Color(0xffFF7F61);
+Color greencolor = Color(0xff3DBD86);
+Color blackcolor = Color(0xff000000);
+Color lightbluecolor = Color(0x1A418FDE);
